9ea1e9dedf09ac8edf1b209c36b699a9eb88c5a6
Adam Konber <konber3@gmail.com>
2024-05-14 18:00:05 -0600

    created functions for hydration data

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,11 @@
 //imports
 import users from './data/users';
 import './css/styles.css';
-import  displayUserInfo from './domUpdates.js'
-
+import displayUserInfo from './domUpdates.js'
+import hydration from './data/hydration.js';
 
 //Global 
+var hydroData = hydration.hydrationData
 var usersArray = users.users
 var randomUser = getUserDataByID(getRandomIndex(usersArray), usersArray)
 var userSteps = avgSteps(usersArray)
@@ -28,11 +29,62 @@ function avgSteps(userArray) {
     return userSteps
 }
 
-// console.log('line28', randomUser)
+function getHydrationData(user, hydroData){
+    var userHydroData = hydroData.filter((userP) => userP.userID === user.id)
+    // console.log('hydroData1:', userHydroData)
+    
+    const hydroAvg = userHydroData.reduce((acc, ounces)=> {
+        acc += ounces.numOunces / userHydroData.length
+        return acc
+    }, 0)
+    
+    // console.log('hydrodata:::', hydroAvg)
+    return hydroAvg
+}
+
+function specificOuncesByDay(date, hydroData){
+    const specificDate = hydroData.filter((day) => day.date === date)   
+    const randomUserOunces = specificDate.find(user => user.userID === randomUser.id)
+
+    // console.log('date:::', specificDate)
+    // console.log('randomUser:::', randomUserOunces)
+    // console.log('userOunce', randomUserOunces.numOunces)
+    return randomUserOunces.numOunces
+}
+
+// function weekOfHydroData(hydroData) {
+//     const weekData = []
+   
+//     console.log('hydroData2::', hydroData)
+//     for(var i = 0; i < 7 && i < hydroData.length; i++) {
+//         weekData.push(hydroData[i].numOunces)
+//     }
+//     console.log('week:::',weekData)
+//     return weekData
+// }
+//the push isnt working 
+
+function weekOfHydroData(user, hydroData) {
+    const userHydroData = hydroData.filter(userP => userP.userID === user.id);
+    const lastIndex = userHydroData.length - 1;
+    const weekData = [];
+
+    const startIndex = lastIndex < 6 ? 0 : lastIndex - 6;
+
+    for (let i = startIndex; i <= lastIndex; i++) {
+        weekData.push(userHydroData[i]);
+    }
+    console.log('line78', weekData)
+    return weekData;
+}
+
+weekOfHydroData(randomUser, hydroData)
+getHydrationData(randomUser, hydroData)
+specificOuncesByDay('2023/06/12', hydroData)
 
 
-displayUserInfo(randomUser)
 
 export { 
     userSteps, 
+    getHydrationData
 }
