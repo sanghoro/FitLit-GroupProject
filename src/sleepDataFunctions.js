4ffefcbbef3a090c9a5d51c9b40445a80f5a5b58
baileyjarvis2814 <baileyjarvis2814@gmail.com>
2024-05-18 15:38:01 -0600

    commit the function for average sleep quality

--- src/sleepDataFunctions.js
+++ src/sleepDataFunctions.js
@@ -10,7 +10,16 @@ function getAverageSleepHours(user, sleepArray) {
     return totalHours / userSleepData.length;
   }
 
+  function getAverageSleepQuality(user, sleepArray) {
+    const userSleepData = sleepArray.filter((userP) => userP.userID === user.id);
+    const totalQuality = userSleepData.reduce(
+      (total, day) => total + day.sleepQuality,
+      0
+    );
+    return totalQuality / userSleepData.length;
+  }
 
 export {
     getAverageSleepHours,
+    getAverageSleepQuality
   };
