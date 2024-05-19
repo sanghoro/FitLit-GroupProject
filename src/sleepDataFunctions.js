cf47b86cd70d99ea30934066a3010aa7e9ed193a
baileyjarvis2814 <baileyjarvis2814@gmail.com>
2024-05-18 18:01:01 -0600

    commit function to get sleep hours per week

--- src/sleepDataFunctions.js
+++ src/sleepDataFunctions.js
@@ -25,8 +25,38 @@ function getAverageSleepHours(user, sleepArray) {
     return specificDate ? specificDate.hoursSlept : 0;
   }
 
+  function specificSleepQualityByDay(date, sleepArray, user) {
+    const userSleepData = sleepArray.filter((entry) => entry.userID === user.id);
+    const specificDate = userSleepData.find((day) => day.date === date);
+    return specificDate ? specificDate.sleepQuality : 0;
+  }
+
+  function sleepHoursForWeek(user, sleepArray, startDate) {
+    const userSleepData = sleepArray.filter((entry) => entry.userID === user.id);
+    const start = new Date(startDate);
+    const end = new Date(startDate);
+    end.setDate(start.getDate() + 6); // End date is 6 days after start date
+  
+    const sleepDataForWeek = userSleepData.filter((entry) => {
+      const entryDate = new Date(entry.date);
+      return entryDate >= start && entryDate <= end;
+    });
+  
+    return sleepDataForWeek.map((entry) => ({
+      date: entry.date,
+      hoursSlept: entry.hoursSlept,
+    }));
+  }
+
+
+  function setSleepData(data) {
+    newSleepData = data;
+  }
 export {
     getAverageSleepHours,
     getAverageSleepQuality,
-    specificSleepHoursByDay
+    specificSleepHoursByDay,
+    specificSleepQualityByDay,
+    sleepHoursForWeek,
+    setSleepData
   };
