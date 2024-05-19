78fea28e98229b652ad483991377af9fe746ac7d
Seong Kang <seong.h.kang2@gmail.com>
2024-05-19 13:41:19 -0500

    iteration4-feat/SplitNames&FixAVGstepGoal

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -17,11 +17,11 @@ export default function displayUserInfo(user) {
   userCard.innerHTML = `
     <section class='user-card'> 
       <h3>User id: #${user.id}</h3>
-      <h3>${user.name}'s daily step goal is ${user.dailyStepGoal} steps</h3>
+      <h3>${user.name.split(" ")[0]}'s daily step goal is ${user.dailyStepGoal} steps</h3>
       <h3>The average step goal is ${userSteps}</h3>
     </section>
   `;
-  welcomeUser.innerText = `Welcome, ${user.name}`;
+  welcomeUser.innerText = `Welcome, ${user.name.split(" ")[0]}`;
 }
 export function displayHydroData(date, weekOfHydro, usersOunces, ouncesByDate) {
   widgetBox.innerHTML = `
@@ -32,19 +32,23 @@ export function displayHydroData(date, weekOfHydro, usersOunces, ouncesByDate) {
     </div>`;
 }
 
-
-export  function displaySleepData(avgSleepHours, avgSleepQuality, sleepHoursByDay, sleepQualityByDay, hoursSleptThisWeek, sleepQualityByWeek) {
+export function displaySleepData(
+  avgSleepHours,
+  avgSleepQuality,
+  sleepHoursByDay,
+  sleepQualityByDay,
+  hoursSleptThisWeek,
+  sleepQualityByWeek
+) {
   widgetBox.innerHTML += `
   <div class='widget widget2'>
   <h3>Avg Sleep: ${Math.round(avgSleepHours)} </h3>
-  <h3>Sleep Quality: ${Math.round(avgSleepQuality)} </h3>
+  <h3>Avg Sleep Quality: ${Math.round(avgSleepQuality)} </h3>
   <h3>Hours slept Today: ${Math.round(sleepHoursByDay)}</h3>
   <h3>Today's sleep quality: ${sleepQualityByDay}</h3>
-  <h3>Hours slept this week: ${hoursSleptThisWeek.map(day => Math.round(day.hoursSlept)).join(', ')} </h3>
-  <h3>Past weeks Sleep Quality: ${sleepQualityByWeek.map(day =>  day.sleepQuality)}</h3>
+  <h3>Hours slept this week: ${hoursSleptThisWeek.map((day) => Math.round(day.hoursSlept)).join(", ")} </h3>
+  <h3>Past weeks Sleep Quality: ${sleepQualityByWeek.map((day) => day.sleepQuality)}</h3>
 </div>`;
 }
-
-
 
 setLoggedInUser();
