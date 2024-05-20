4377ad10a470674f3f084f2eb2e3f08c2a2777fa
Seong Kang <seong.h.kang2@gmail.com>
2024-05-19 20:22:22 -0500

    playing with CSS

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -22,32 +22,30 @@ export default function displayUserInfo(user, userData) {
 }
 
 function checkIfDisplayed(user, userData) {
+  userCard.innerHTML = "";
   if (isAllUserInfoDisplayed) {
     const friends = findFriends(user, userData);
     const friendsNames = friends.map((friend) => friend.name).join(", ");
-
     userCard.innerHTML = `
       <section class='allInfoCard'>
-      <h3>User id: #${user.id}</h3> 
-      <h3>Full name: ${user.name}</h3>
-      <h3>Email: ${user.email}</h3>
-      <h3>Address: ${user.address}</h3>
-      <h3>Friends: ${friendsNames}</h3>
-      <h3>Daily Step Goal: ${user.dailyStepGoal}</h3>
-      <h3>Stride Length: ${user.strideLength}</h3>
+        <h3>User id: #${user.id}</h3> 
+        <h3>Full name: ${user.name}</h3>
+        <h3>Email: ${user.email}</h3>
+        <h3>Address: ${user.address}</h3>
+        <h3>Friends: ${friendsNames}</h3>
+        <h3>Daily Step Goal: ${user.dailyStepGoal}</h3>
+        <h3>Stride Length: ${user.strideLength}</h3>
+        <button class='moreInfoBttn'>Hide</button>
       </section>
-      <button class='moreInfoBttn'>Hide</button>
     `;
-    userCard.classList.add("allInfoCard");
   } else {
-    userCard.classList.remove("allInfoCard");
     userCard.innerHTML = `
       <section class='user-card'> 
         <h3>User id: #${user.id}</h3>
         <h3>${user.name.split(" ")[0]}'s daily step goal is ${user.dailyStepGoal} steps</h3>
         <h3>The average step goal is ${userSteps}</h3>
+        <button class='moreInfoBttn'>More User Info</button>
       </section>
-      <button class='moreInfoBttn'>More User Info</button>
     `;
   }
   var infoBttn = document.querySelector(".moreInfoBttn");
@@ -79,7 +77,7 @@ export function displaySleepData(
   widgetBox.innerHTML += `
   <div class='widget widget2'>
   <h3>Avg Sleep: ${Math.round(avgSleepHours)} </h3>
-  <h3>Avg Sleep Quality: ${avgSleepQuality.toFixed(2)} </h3> <!-- Use toFixed(2) to display properly -->
+  <h3>Avg Sleep Quality: ${avgSleepQuality.toFixed(2)} </h3>
   <h3>Hours slept Today: ${Math.round(sleepHoursByDay)}</h3>
   <h3>Today's sleep quality: ${sleepQualityByDay}</h3>
   <h3>Hours slept this week: ${hoursSleptThisWeek.map((day) => Math.round(day.hoursSlept)).join(", ")} </h3>
